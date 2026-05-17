846ceb784f1449c01cfba718f56f09c64587167f
EC2 Default User <ec2-user@ip-172-31-20-98.ec2.internal>
2026-05-17 20:00:24 +0000

    test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD Pipeline! AMIT Ojha");
+        System.out.println("Hello from Jenkins CI/CD Pipeline! AMIT Ojha !!!");
     }
 }
